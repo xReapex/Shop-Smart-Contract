5508d66996b8b367d97c6fd306d18a54d6161f2f
xReapex <tr7296228@gmail.com>
2025-03-13 12:21:36 +0100

    add basic test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,6 @@ const ShopManager = artifacts.require('./ShopManager');
 const Manager = artifacts.require('./Manager');
 
 module.exports = function (deployer) {
-  deployer
-    .deploy(Manager)
-    .deploy(ShopManager);
+  deployer.deploy(Manager);
+  deployer.deploy(ShopManager);
 };
